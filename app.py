4839cbdf483d76364b98ab9be1fc82445d08f9a4
JairoSG <jairosandovalg011@gmail.com>
2026-08-12 15:18:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 file = st.file_uploader("Sube tu archivo", type = ["csv"])
 
-st.download_button(
-      label="Descargar archivo modificado",
-      data=csv_data,
-      file_name="archivo_modificado.csv",
-      mime="text/csv",
-  )
+#st.download_button(
+#      label="Descargar archivo modificado",
+#      data=csv_data,
+#      file_name="archivo_modificado.csv",
+#      mime="text/csv",
+#  )
