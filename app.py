7ee048dd728a0734da29e6a4712143ccf37e2a57
JairoSG <jairosandovalg011@gmail.com>
2026-08-12 16:08:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 st.title("Cargar y Descargar Archivo")
 st.sidebar.header("Panel de Control")
 
-menu = st.sidebar.selectbox(
-                            "Navegacion",  
-                            [
-                            "Opcion 1",
-                            "Opcion 2",
-                            "Opcion 3"]
-                           )
+with st.sidebar:
+  st.header("Panel de Control")
 
+  # Menú con iconos y selector de fondo activo
+  selected = option_menu(
+      menu_title="Navegacion",  # Título del menú
+      options=[
+          "Modificación de archivos",
+          "Análisis de Datos",
+          "Reportes",
+      ],  # Tus 3 opciones
+      icons=["file-earmark-arrow-up", "bar-chart", "file-text"],  # Iconos opcionales de Bootstrap
+      menu_icon="cast",
+      default_index=0,
+  )
+  
 st.markdown("---")
 
 file = st.file_uploader("Sube tu archivo", type=["csv"])
